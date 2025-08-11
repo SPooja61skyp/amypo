606c2b46a45fdc22543f71f4fd299c27096f9f09
Pooja S <poojasaravananskyp@gmail.com>
2025-08-11 12:07:52 +0530

    both practice and workout iterativestatements added in java

---
+++ amypo/python/4-Iterative-Statements/practice/itp-1.py
@@ -0,0 +1,10 @@
+try:
+    n=int(input())
+    if n<0:
+        print("Invalid input")
+    else:
+        for i in range(1,n+1):
+            if i%3!=0:
+                print(i)
+except:
+    print("Invalid input")
