3a75b293190b89dc74b6c206311893f9bd22918d
Pooja S <poojasaravananskyp@gmail.com>
2025-08-12 10:33:26 +0530

    both practice and workout added in python

---
+++ amypo/python/10-oops/practice/oopsp-2.py
@@ -0,0 +1,14 @@
+name = input().strip()
+age_input = input().strip()
+if len(name) < 1 or len(name) > 100:
+    print("Invalid input")
+else:
+    try:
+        age = int(age_input)
+        if 10 <= age <= 150:
+            print(name)
+            print(age)
+        else:
+            print("Invalid input")
+    except:
+        print("Invalid input")
---
+++ amypo/python/11-Inheritance-and-Polymorphism/workout/inhw-1.py
@@ -0,0 +1,10 @@
+signal = input().strip()
+
+if signal == "Red":
+    print("Stop")
+elif signal == "Yellow":
+    print("Ready")
+elif signal == "Green":
+    print("Go")
+else:
+    print("Invalid input")
